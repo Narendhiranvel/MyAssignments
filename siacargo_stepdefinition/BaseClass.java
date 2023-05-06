49b36e5adcab4bb9444459fc24c853105f6be01a
Narendhiranvel <66551636+Narendhiranvel@users.noreply.github.com>
2023-05-06 23:38:31 +0530

    Delete siacargo_stepdefinition directory

--- siacargo_stepdefinition/BaseClass.java
+++
@@ -1,8 +0,0 @@
-package siacargo_stepdefinition;
-
-import org.openqa.selenium.edge.EdgeDriver;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-
-public class BaseClass extends AbstractTestNGCucumberTests {
-	public static EdgeDriver driver;
-}
